010aeda836ef4159e902698ccee28953e88b7c3c
Scott Burch <scott@bulldoginfo.com>
2016-07-05 19:51:33 -0700

    small cleanups

--- test/spec/reactiveStore/dumpSpec.js
+++ test/spec/reactiveStore/dumpSpec.js
@@ -3,7 +3,6 @@ var ReactiveStore = require('src/ReactiveStore');
 describe('ReactiveStore.dump()', ()=>{
     var rs;
     var date = new Date();
-    var o;
 
     beforeEach(()=>{
         rs = ReactiveStore();
@@ -14,23 +13,23 @@ describe('ReactiveStore.dump()', ()=>{
     });
 
     it('should dump an object with the values', ()=>{
-        o = rs.dump();
+        var o = rs.dump();
         expect(o.value1).toBe('a value');
         expect(o.some.deep.value).toBe('another');
     });
 
     it('should serialize dates using ISO string', ()=>{
-        o = rs.dump();
+        var o = rs.dump();
         expect(o.date).toBe(date.toISOString());
     });
 
     it('should dump an object with arrays', ()=>{
-        o = rs.dump();
+        var o = rs.dump();
         expect(o.arr[1]).toBe(2);
     });
 
     it('should dump an object with an (optional) starting point', ()=>{
-        o = rs.dump('some.deep.value');
+        var o = rs.dump('some.deep.value');
         expect(o).toBe('another');
         o = rs.dump('some.deep');
         expect(o.value).toEqual('another');
